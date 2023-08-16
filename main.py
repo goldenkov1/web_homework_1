a0feaf9f673512e721180f536750bf99b48c9015
goldenkov slava <oldfild1@gmail.com>
2023-08-16 07:55:37 +0300

    change code

--- main.py
+++ main.py
@@ -376,20 +376,21 @@ class MyServer(BaseHTTPRequestHandler):
         """
     def do_GET(self):
         query_components = parse_qs(urlparse(self.path).query)
-        page_content = self.get_html_content()
+        page_content = self.__get_html_content()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wrile.write(bytes(page_content, "utf-8"))
+        self.wfile.write(bytes(page_content, "utf-8"))
 
-    if __name__ == "__main__":
-        webServer = HTTPServer((hostName, serverPort), MyServer)
-        print("Server started http://%s:%s" % (hostName, serverPort))
 
-        try:
-            webServer.serve_forever()
-        except KeyboardInterrupt:
-            pass
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
 
-        webServer.server_close()
-        print("Server stopped.")
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
